91d0aa0492e989aa1439fd3e7ebd1f50aa5189fb
Faysal Ahamed <miufaysal@live.com>
2019-07-23 16:42:21 +0600

    wip

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -127,18 +127,18 @@
                                         <i class="fa fa-th-large"></i> Dashboard </a>
                                 </li>
                                 @can('manage_status')
-                                <li class="">
+                                <li class="{{(request()->is('admin/statuses*')) ? 'active' : '' }}">
                                     <a href="/admin/statuses">
                                         <i class="fa fa-tag"></i> Statuses </a>
                                 </li>
                                 @endcan
                                 @can('manage_source')
-                                <li class="">
+                                <li class="{{(request()->is('admin/sources*')) ? 'active' : '' }}">
                                     <a href="/admin/sources">
                                         <i class="fa fa-external-link"></i> Source </a>
                                 </li>
                                 @endcan
-                                <li class="">
+                                <li class="{{(request()->is('admin/leads*')) ? 'active' : '' }}">
                                     <a href="/admin/leads">
                                         <i class="fa fa-users"></i> Leads </a>
                                 </li>
