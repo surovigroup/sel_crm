764b43ca96523244ff9ae16632dc5c3901833ee4
Faysal Ahamed <miufaysal@live.com>
2019-11-21 13:45:56 +0600

    wip

--- resources/views/vendor/laravel-admin/form/fields/text.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group @error($id) has-error @enderror">
-    {{ Form::label( $id, $title, ['class' => 'control-label']) }}
-    {{ Form::text( $id, null, ['class' => 'form-control' .($errors->has($id) ? ' is-invalid':'')] ) }}
-    @error($id)
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-</div>
