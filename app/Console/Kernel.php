9493531bb9891e4e408d3b7285c6d0c3a9f11536
Faysal Ahamed <miufaysal@live.com>
2019-12-11 14:57:03 +0600

    Backup schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('backup:run')->daily()->at(env('BACKUP_SCHEDULE'));
         // $schedule->command('inspire')
         //          ->hourly();
     }
