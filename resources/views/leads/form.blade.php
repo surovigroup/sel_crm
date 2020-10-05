b2b032dd15df68e7b02064df0320e72b386eedd7
Faysal Ahamed <miufaysal@live.com>
2020-10-05 20:26:11 +0600

    laravel admin update, form fields to blade components

--- resources/views/leads/form.blade.php
+++ resources/views/leads/form.blade.php
@@ -1,101 +1,47 @@
 <div class="row">
     <div class="col-md-12">
-        @include('laravel-admin::form.text', [
-            'name'  => 'name',
-            'label' => 'Name',
-            'value' => $lead->name
-        ])
+        <x-text-field name="name" label="Name" :value="$lead->name" />
     </div>
 </div>
 <div class="row">
     <div class="col-md-6">
-        @include('laravel-admin::form.text', [
-            'name'  => 'phone',
-            'label' => 'Phone',
-            'value' => $lead->phone
-        ])
+        <x-text-field name="phone" label="Phone" :value="$lead->phone" />
     </div>
     <div class="col-md-6">
-        @include('laravel-admin::form.email', [
-            'name'  => 'email',
-            'label' => 'Email',
-            'value' => $lead->email
-        ])
+        <x-email-field name="email" label="Email" :value="$lead->email" />
     </div>
 </div>
 <div class="row">
     <div class="col-md-6">
-        @include('laravel-admin::form.text', [
-            'name'  => 'description',
-            'label' => 'Description',
-            'value' => $lead->description
-        ])
+        <x-text-field name="description" label="Description" :value="$lead->description" />
     </div>
     <div class="col-md-6">
-        @include('laravel-admin::form.text', [
-            'name'  => 'company',
-            'label' => 'Company Name',
-            'value' => $lead->company
-        ])
+        <x-text-field name="company" label="Company" :value="$lead->company" />
     </div>
 </div>
 <div class="row">
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'division',
-            'label' =>  'Division',
-            'value' =>  $lead->division,
-            'data'  =>  $divisions
-        ])
+        <x-select-field name="division" label="Division" :value="$lead->division" :data="$divisions" />
     </div>
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'district',
-            'label' =>  'District',
-            'value' =>  $lead->district,
-            'data'  =>  $districts
-        ])
+        <x-select-field name="district" label="District" :value="$lead->district" :data="$districts" />
     </div>
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'upazila',
-            'label' =>  'Upazila',
-            'value' =>  $lead->upazila,
-            'data'  =>  $upazilas
-        ])
+        <x-select-field name="upazila" label="Upazila" :value="$lead->upazila" :data="$upazilas" />
     </div>
 </div>
 <div class="row">
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'source',
-            'label' =>  'Source',
-            'value' =>  $lead->source,
-            'data'  =>  $sources
-        ])
+        <x-select-field name="source" label="Source" :value="$lead->source" :data="$sources" />
     </div>
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'status_id',
-            'label' =>  'Status',
-            'value' =>  $lead->status_id,
-            'data'  =>  $statuses
-        ])
+        <x-select-field name="status_id" label="Status" :value="$lead->status_id" :data="$statuses" />
     </div>
     <div class="col-md-4">
-        @include('laravel-admin::form.select', [
-            'name'  =>  'admin_assigned_id',
-            'label' =>  'Assign to',
-            'value' =>  $lead->admin_assigned_id,
-            'data'  =>  $lead_managers
-        ])
+        <x-select-field name="admin_assigned_id" label="Assign to" :value="$lead->admin_assigned_id" :data="$lead_managers" />
     </div>
     <div class="col-md-12">
-        @include('laravel-admin::form.text', [
-            'name'  =>  'note',
-            'label' =>  'Notes',
-            'value' =>  $lead->note,
-        ])
+        <x-text-field name="note" label="Notes" :value="$lead->note" />
     </div>
 </div>
 
