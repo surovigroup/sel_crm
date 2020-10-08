840a162a24d6d29c6a9dd7ff95503f23d8981146
Faysal Ahamed <miufaysal@live.com>
2020-10-08 11:29:03 +0600

    laravel-admin update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,67 +2,14 @@
 @section('content')
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script>
-<section class="section">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-xl-12 stats-col">
-            <div class="card stats" data-exclude="xs">
-                <div class="card-header card-header-sm bordered">
-                    <div class="header-block">
-                        <h3 class="title">At a Glance</h3>
-                    </div>
-                </div>
-                <div class="card-block">
-                    <div class="row row-sm stats-container">
-                        <div class="col-12 col-sm-3  stat-col">
-                            <div class="custom-box">
-                                <div class="stat-icon">
-                                    <i class="fa fa-building"></i>
-                                </div>
-                                <div class="stat">
-                                    <div class="value"> {{ $leadCount }} </div>
-                                    <div class="name"> Leads </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-3 stat-col">
-                            <div class="custom-box">
-                                <div class="stat-icon">
-                                    <i class="fa fa-sitemap"></i>
-                                </div>
-                                <div class="stat">
-                                    <div class="value"> {{ $sourceCount }} </div>
-                                    <div class="name"> Sources </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-3 stat-col">
-                            <div class="custom-box">
-                                <div class="stat-icon">
-                                    <i class="fa fa-check-circle-o"></i>
-                                </div>
-                                <div class="stat">
-                                    <div class="value"> {{ $order_confirmed }} </div>
-                                    <div class="name"> Order Confirmed </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-3 stat-col">
-                            <div class="custom-box">
-                                <div class="stat-icon">
-                                    <i class="fa fa-check-circle"></i>
-                                </div>
-                                <div class="stat">
-                                    <div class="value"> {{ $order_completed }} </div>
-                                    <div class="name"> Order Completed </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+
+<x-laravel-admin::stats-section title="At a Glance">
+    <x-laravel-admin::stats-item :count="$leadCount" label="Leads" icon="fa fa-building"/>
+    <x-laravel-admin::stats-item :count="$sourceCount" label="Sources" icon="fa fa-sitemap"/>
+    <x-laravel-admin::stats-item :count="$order_confirmed" label="Order Confirmed" icon="fa fa-check-circle-o"/>
+    <x-laravel-admin::stats-item :count="$order_completed" label="Order Completed" icon="fa fa-check-circle"/>
+</x-laravel-admin::stats-section>
+
 <section class="section map-tasks">
     <div class="row sameheight-container">
         <div class="col-md-12">
